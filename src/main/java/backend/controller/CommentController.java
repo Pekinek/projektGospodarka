c215a10f4dee497aa00ba5124a18261bb75741f4
mmocek <michal.mocek@nokia.com>
2016-12-13 12:17:31 +0100

    comments added

---
+++ src/main/java/backend/controller/CommentController.java
@@ -0,0 +1,46 @@
+package backend.controller;
+
+import backend.model.Comment;
+import backend.model.CommentSummary;
+import backend.repository.CommentRepository;
+import backend.repository.OfferRepository;
+import backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+public class CommentController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    OfferRepository offerRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @CrossOrigin
+    @RequestMapping("/offers/{id}/addComment")
+    public ResponseEntity<String> addComment(@PathVariable("id") Integer offerId, @RequestHeader("Authorization") String token, @RequestBody Comment comment){
+        comment.setDate(new Date().toString());
+        comment.setOffer(offerRepository.findOne(offerId));
+        comment.setUser(userRepository.findByToken(token).get(0));
+        commentRepository.save(comment);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @RequestMapping("/offers/{id}/getComments")
+    public ResponseEntity<Iterable<CommentSummary>> getCommentsByOffer(@PathVariable("id") Integer offerId){
+        Set<Comment> comments = offerRepository.findOne(offerId).getComments();
+        Set<CommentSummary> commentSummaries = comments.stream().map(c -> c.toCommentSummary()).collect(Collectors.toSet());
+        return new ResponseEntity(commentSummaries, HttpStatus.OK);
+    }
+}
--- src/main/java/backend/model/Comment.java
+++ src/main/java/backend/model/Comment.java
@@ -1,45 +1,36 @@
 package backend.model;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.Set;
 
 @Entity
 @Getter
 @Setter
-public class Offer {
+public class Comment {
 
     @JsonIgnore
     @ManyToOne
     private User user;
 
     @JsonIgnore
-    @OneToMany(mappedBy = "offer")
-    private Set<Comment> comments;
+    @ManyToOne
+    private Offer offer;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    @NotNull
-    private String title;
-    @NotNull
-    private String description;
-    @NotNull
-    private String type;
-    @NotNull
-    private String place;
-    @NotNull
-    private String purpose;
-    @NotNull
-    private String price;
-    private String date;
-    
-    @Column( length = 1000000000 )
-    private byte[] pictures;
 
+    @NotNull
+    private String date;
+
+    @NotNull
+    private String message;
+
+    public CommentSummary toCommentSummary(){
+        return new CommentSummary(user.getLogin(), offer.getId(), date, message);
+    }
 }
