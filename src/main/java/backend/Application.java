9da21b678f0e666135c11a57db4cbd7dd47a3d90
mejdej93 <marcinmadej1993@gmail.com>
2016-12-14 22:43:59 +0100

    admin password fix

--- src/main/java/backend/Application.java
+++ src/main/java/backend/Application.java
@@ -37,7 +37,7 @@ public class Application implements CommandLineRunner {
         admin.setType("admin");
         admin.setTelephone("");
         admin.setLogin("admin");
-        admin.setPassword("C7AD44CBAD762A5DA0A452F9E854FDC1E0E7A52A38015F23F3EAB1D80B931DD472634DFAC71CD34EBC35D16AB7FB8A90C81F975113D6C7538DC69DD8DE9077EC");
+        admin.setPassword("c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec");
         userRepository.save(admin);
     }
 }
