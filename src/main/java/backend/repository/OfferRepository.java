5f6f271c87472a4de4a88616d677b972b8ba4baa
mejdej93 <marcinmadej1993@gmail.com>
2017-01-08 21:49:19 +0100

    offer filters

--- src/main/java/backend/repository/OfferRepository.java
+++ src/main/java/backend/repository/OfferRepository.java
@@ -2,6 +2,7 @@ package backend.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -10,11 +11,28 @@ import backend.model.Offer;
 import backend.model.User;
 
 @RepositoryRestResource(exported = false)
-public interface OfferRepository extends PagingAndSortingRepository<Offer, Integer> {
-	Page<Offer> findByUser(@Param("user") User user, Pageable pageable);
+public interface OfferRepository extends
+		PagingAndSortingRepository<Offer, Integer> {
 
-    Page<Offer> findByArchived(@Param("archived") Boolean archived, Pageable pageable);
-    
-    Page<Offer> findByUserAndArchived(@Param("user") User user, @Param("archived") Boolean archived,  Pageable pageable);
+	@Query("select c from Offer c where c.user.login like %?1"
+			+ " and c.title like %?2 " + " and c.type like %?3"
+			+ " and c.place like %?4" + " and c.purpose like %?5"
+			+ " and c.price <= ?6")
+	Page<Offer> findByUserAndFilter(@Param("login") String login,
+			@Param("title") String title, @Param("type") String type,
+			@Param("place") String place, @Param("purpose") String purpose,
+			@Param("price") String price, Pageable pageable);
+
+	@Query("select c from Offer c where c.title like %?1"
+			 + " and c.type like %?2"
+			+ " and c.place like %?3" + " and c.purpose like %?4"
+			+ " and c.price <= ?5")
+	Page<Offer> findByFilter(@Param("title") String title,
+			@Param("type") String type, @Param("place") String place,
+			@Param("purpose") String purpose, @Param("price") String price,
+			Pageable pageable);
+
+	Page<Offer> findByUserAndArchived(@Param("user") User user,
+			@Param("archived") Boolean archived, Pageable pageable);
 
 }
