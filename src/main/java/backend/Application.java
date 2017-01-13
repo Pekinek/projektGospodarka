e261d48138c87b1a2abd026b1d2b8be994dc0a97
mejdej93 <marcinmadej1993@gmail.com>
2017-01-13 17:45:00 +0100

    fixes

--- src/main/java/backend/Application.java
+++ src/main/java/backend/Application.java
@@ -36,12 +36,12 @@ public class Application implements CommandLineRunner {
 
 	private void addAdmin() {
 		User admin = new User();
-		admin.setFirstName("");
-		admin.setLastName("");
-		admin.setEmail("");
+		admin.setFirstName("Marcin");
+		admin.setLastName("Madej");
+		admin.setEmail("marcinmadej1993@gmail.com");
 		admin.setToken("f4f673cf-08d2-4e60-ad7c-5509d73a5688");
 		admin.setType("admin");
-		admin.setTelephone("");
+		admin.setTelephone("535279542");
 		admin.setLogin("admin");
 		admin.setEnabled(true);
 		admin.setPassword("c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec");
